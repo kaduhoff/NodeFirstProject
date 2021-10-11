4d594cd93003f57d3aa046e2815c8f673945214b
Kadu Hoffmann <kadu@cabopec.com.br>
2021-10-11 01:36:29 +0000

    new add to our lessons

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
 const path = require("path");
 
 const app = express();
 
+app.set("view engine", "ejs");
+//app.set("layout", "./layout")
+app.use(expressLayouts); 
 app.use('/public', express.static(path.join(__dirname, 'public')))
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.render('loginForm');
+    //res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 //habilita o parse dos application/x-www-form-urlencoded 
 app.use(express.urlencoded({extended: false}));
@@ -14,6 +19,11 @@ app.use(express.urlencoded({extended: false}));
 //var urlencodedParser = express.urlencoded({ extended: false })
 app.post('/login', (req, res) => {
     res.send(req.body.email + "post enviado");
+});
+
+app.get('/lista', (req, res) => {
+    const data = require('./public/listaCompras.json');
+    res.send(JSON.stringify(data));
 });
 
 app.listen(48081, () => {
